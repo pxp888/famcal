5e5060f02b675df68233537d135825da15fca559
pxp888 <pxperrine@gmail.com>
2024-01-06 23:43:17 +0100

    add getCalendars and getEvents methods

---
+++ app.py
@@ -0,0 +1,109 @@
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import os
+import datetime
+import pp
+
+def say(*args):
+	pp(args)
+
+print = say
+
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly","https://www.googleapis.com/auth/calendar.readonly"]
+
+
+
+def getCalendars(creds):
+	try:
+		service = build("calendar", "v3", credentials=creds)
+		print("Getting the list of calendars")
+		calendars_result = service.calendarList().list().execute()
+
+		calendars = calendars_result.get('items', [])
+
+		if not calendars:
+			print('No calendars found.')
+			return None
+		
+		return calendars
+
+		for calendar in calendars:
+			summary = calendar['summary']
+			id = calendar['id']
+			primary = "Primary" if calendar.get('primary') else ""
+			print("%s\t%s\t%s" % (summary, id, primary))
+
+	except HttpError as error:
+		print(f"An error occurred: {error}")
+		return None
+
+
+def getEvents(creds, calendarId):
+	try:
+		service = build("calendar", "v3", credentials=creds)
+		print(f"Getting the upcoming 10 events from calendar {calendarId}")
+		now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+		events_result = (
+			service.events()
+			.list(
+				calendarId=calendarId,
+				timeMin=now,
+				maxResults=10,
+				singleEvents=True,
+				orderBy="startTime",
+			)
+			.execute()
+		)
+		events = events_result.get("items", [])
+
+		if not events:
+			print("No upcoming events found.")
+			return
+
+		# Prints the start and name of the next 10 events
+		for event in events:
+			start = event["start"].get("dateTime", event["start"].get("date"))
+			print(start, event["summary"])
+
+	except HttpError as error:
+		print(f"An error occurred: {error}")
+
+
+def main():
+	"""Shows basic usage of the Google Calendar API.
+	Prints the start and name of the next 10 events on the user's calendar.
+	"""
+	creds = None
+	# The file token.json stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists("token.json"):
+		creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+			"credentials.json", SCOPES
+			)
+		creds = flow.run_local_server(port=0)
+		# Save the credentials for the next run
+		with open("token.json", "w") as token:
+			token.write(creds.to_json())
+
+
+	cals = getCalendars(creds)
+	if cals:
+		for cal in cals:
+			print(cal['summary'])
+			getEvents(creds, cal['id'])
+
+
+if __name__ == "__main__":
+	main()
